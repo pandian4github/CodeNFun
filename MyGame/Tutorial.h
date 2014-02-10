b679c58744020ae887bc60977ab923e4b80b01cc
Pandian Raju <pandian4mail@gmail.com>
2014-02-10 22:33:24 +0530

    First commit in pandian's laptop

---
+++ MyGame/Tutorial.h
@@ -0,0 +1,63 @@
+#ifndef TUTORIAL_H
+#define TUTORIAL_H
+#include "SFML\Graphics.hpp"
+#include "SFML\Audio.hpp"
+#include <iostream>
+
+sf::Sprite *tutsprite;
+sf::Texture tuttex;
+sf::Music musicBuffer;
+bool playing;
+    
+void init_tutorial(int tutorialLevel,int tutorialNo)
+{
+	
+	std::string imageLocation = "Assets/tut";
+	imageLocation += std::to_string(tutorialLevel);
+	imageLocation += "_";
+	imageLocation += std::to_string(tutorialNo);
+	imageLocation += ".png";
+
+	std::cout<<imageLocation;
+
+	if(!tuttex.loadFromFile(imageLocation))
+	{
+		std::cout<<"\nError loading tut";
+	}
+	tutsprite=new sf::Sprite();
+	tutsprite->setTexture(tuttex);
+	tutsprite->setTextureRect(sf::IntRect(0,0,1000,562));
+	tutsprite->setPosition(0,0);
+
+	std::string soundLocation = "Assets/sound";
+	soundLocation += std::to_string(tutorialLevel);
+	soundLocation += "_";
+	soundLocation += std::to_string(tutorialNo);
+	soundLocation += ".wav";
+
+	std::cout<<soundLocation;
+
+	if (!musicBuffer.openFromFile(soundLocation))
+	{
+       std::cout<<"\nError loading sound file";
+	}
+
+	playing = false;
+}
+
+void show_tutorial( sf::RenderWindow *window)
+{
+	if(playing==false)
+	{
+		musicBuffer.play();
+		playing=true;
+	}
+	window->draw(*tutsprite);
+}
+
+void end_tutorial()
+{
+	playing=false;
+	musicBuffer.stop();
+}
+#endif
---
+++ MyGame/main.cpp
@@ -0,0 +1,180 @@
+// Tankie.cpp : Defines the entry point for the console application.
+//
+
+#include <SFML/Graphics.hpp>
+#include "iostream"
+#include "hero.h"
+#include "CollRectManager.h"
+#include "Eva.h"
+#include "Tutorial.h"
+
+const int LEVEL = 1;
+const int TUTORIAL = 0;
+sf::RenderWindow *window;
+
+std::string getBgLocation(int level)
+{
+	std::string bgLocation = "Assets/background";
+	bgLocation += std::to_string(level);
+	bgLocation += ".png";
+	return bgLocation;
+}
+
+void otherDisplays(int level)
+{
+	if(level == 1)
+	{	
+		animate_eva();
+		display_eva(window);
+	}
+}
+void otherInitializations(int level)
+{
+	if(level == 1)
+	{
+		Init_Eva(50,200);
+	}
+}
+int main()
+{
+ 
+	window=new sf::RenderWindow(sf::VideoMode(1000,562), "Final Year Project",sf::Style::Close);
+	window->setFramerateLimit(20);
+
+	int SCENE = TUTORIAL;
+	int level = 0, tutorial = 1;
+	int noOfTutorials[3] = {2,3,3};
+	int collisionRects[3]={0,5,2};
+	
+	sf::Sprite *bgsprite = new sf::Sprite;
+	sf::Texture bgtex;
+	Hero *myHero;
+
+	//BACKGROUND
+
+	/*if(levelChanged)
+	{
+		if(!bgtex.loadFromFile("Assets/background1.png"))
+		{
+			std::cout<<"\nError loading background";
+		}
+		
+		bgsprite->setTexture(bgtex);
+		bgsprite->setTextureRect(sf::IntRect(0,0,1000,562));
+
+		//Load tutorial
+		Init_Eva(50,200);
+		init_tutorial(level,tutorial);
+		
+		//HERO
+		myHero=new Hero(700,300);
+		myHero->setLevel(level);
+		myManager->setTotalRects(collisionRects[level]);
+		myManager->initRects(level);
+		
+		levelChanged = 0;
+	}*/
+
+	init_tutorial(level,tutorial);
+
+    while (window->isOpen())
+    {
+        sf::Event event;
+        while (window->pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window->close();
+			else if(sf::Keyboard::isKeyPressed(sf::Keyboard::T))
+			{
+				if(SCENE!=TUTORIAL)
+					SCENE=TUTORIAL;
+			}
+			else if(sf::Keyboard::isKeyPressed(sf::Keyboard::U))
+			{
+				if(SCENE == TUTORIAL)
+				{
+					end_tutorial();
+					if(level == 0)
+						levelChanged =1;
+					SCENE=LEVEL;
+				}
+			}
+			else if(sf::Keyboard::isKeyPressed(sf::Keyboard::N))
+			{
+				if(SCENE == TUTORIAL)
+				{
+					++tutorial;
+					if(tutorial > noOfTutorials[level])
+					{
+						--tutorial;
+					}
+					else
+					{
+						init_tutorial(level,tutorial); //move through tutorials
+					}
+				}
+			}
+			else if(sf::Keyboard::isKeyPressed(sf::Keyboard::B))
+			{
+				if( SCENE == TUTORIAL )
+				{
+					--tutorial;
+					if(tutorial<1)
+					{
+						++tutorial;
+					}
+					else
+					{
+						init_tutorial(level,tutorial); //move through tutorials
+					}
+				}
+			}
+        }
+
+		if(levelChanged)
+		{
+			level++;
+			tutorial = 1;
+
+			std::string bgLocation = getBgLocation(level);
+
+			if(!bgtex.loadFromFile(bgLocation))
+			{
+				std::cout<<"\nError loading background";
+			}
+
+			bgsprite->setTexture(bgtex);
+			bgsprite->setTextureRect(sf::IntRect(0,0,1000,562));
+
+			init_tutorial(level,tutorial);
+			otherInitializations(level);
+			//HERO
+			myHero = new Hero(700,300);
+			myHero->setLevel(level);
+			myManager->setTotalRects(collisionRects[level]);
+			myManager->initRects(level);
+
+			levelChanged = 0;
+		}
+
+		switch(SCENE)
+		{
+		case LEVEL:
+				myHero->handle_input();
+				myHero->update(myManager);
+				window->clear();
+				window->draw(*bgsprite);
+				otherDisplays(level);
+				myHero->display(window);
+				break;
+		case TUTORIAL:
+				window->clear();
+				show_tutorial(window);
+				break;
+		}
+		window->display();
+    }
+
+	return 0;
+}
+
