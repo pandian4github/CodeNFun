e0534d6d846f729a1ca85c172fc01896bd6ad5b1
Pandian Raju <pandian4mail@gmail.com>
2014-02-10 22:50:21 +0530

    deleted files added again

---
+++ MyGame/Eva.h
@@ -0,0 +1,44 @@
+#ifndef EVA_H
+#define EVA_H
+#include "SFML\Graphics.hpp"
+#include "CollRectManager.h"
+#include <iostream>
+sf::Sprite *evasprite;
+sf::Texture evatex;
+int frame_count;
+void Init_Eva(int x,int y)
+{
+	 
+	 if(!evatex.loadFromFile("Assets/robo.png"))
+	{
+		std::cout<<"\nError loading robo";
+	}
+	evasprite=new sf::Sprite();
+	evasprite->setTexture(evatex);
+	evasprite->setTextureRect(sf::IntRect(0,0,150,200));
+	evasprite->setPosition((float)x,(float)y);
+
+	frame_count=0;
+}
+
+void animate_eva()
+{
+	
+	frame_count=(frame_count+1)%10;
+	if(frame_count<5)
+		evasprite->move(0,-1);
+	else
+		evasprite->move(0,+1);
+}
+void display_eva( sf::RenderWindow *window)
+{
+	window->draw(*evasprite);
+}
+
+void changeEvaPosition(int level)
+{
+	if(level==1)
+		myManager->chanagePosition(0,0,0,0,3); //3rd collision rectangle needs to be changed for level 1
+	evasprite->setPosition(0,0);
+}
+#endif
---
+++ MyGame/externalFunctions.h
@@ -0,0 +1,82 @@
+#ifndef EXTERNALFUNCTIONS_H
+#define EXTERNALFUNCTIONS_H
+
+#include<iostream>
+#include<cstring>
+#include<fstream>
+#include "Eva.h"
+
+std::string exec(char* cmd) {
+    FILE* pipe = _popen(cmd, "r");
+    if (!pipe) return "ERROR";
+    char buffer[128];
+    std::string result = "";
+    while(!feof(pipe)) {
+    	if(fgets(buffer, 128, pipe) != NULL)
+    		result += buffer;
+    }
+    _pclose(pipe);
+    return result;
+}
+
+void openCodingArea(int level)
+{
+	//write your function here
+	char command[1024];
+
+	std::string cmd = "C:\\Dev-Cpp\\devcpp.exe C:\\Users\\FYP\\";
+	cmd += "codeForLevel" + std::to_string(level) + ".cpp";
+
+	std::cout<<"Start Coding";
+	strcpy(command,cmd.c_str());
+	std::cout<<exec(command);
+}
+
+bool compileCode(int level)
+{
+	//write your function here
+	std::cout<<"Compile Code";
+
+	char command[1024];
+	std::string cmd,ret;
+
+	//create .exe
+	cmd = "C:\\Dev-Cpp\\bin\\g++.exe C:\\Users\\FYP\\";
+	cmd += "codeForLevel" + std::to_string(level) + ".cpp -o C:\\Users\\FYP\\execForLevel" + std::to_string(level);
+	strcpy(command,cmd.c_str());
+	std::cout<<exec(command);
+
+	//run the .exe
+	cmd = "C:\\Users\\FYP\\execForLevel";
+	cmd += std::to_string(level) + ".exe > C:\\Users\\FYP\\solForLevel" + std::to_string(level) + ".txt";
+	strcpy(command,cmd.c_str());
+	std::cout<<exec(command);
+
+	//compare the files
+	cmd = "FC C:\\Users\\FYP\\solForLevel" + std::to_string(level) + ".txt" + " C:\\Users\\FYP\\correctSolForLevel" + std::to_string(level) + ".txt" + " > C:\\Users\\FYP\\diff.txt";
+	strcpy(command,cmd.c_str());
+	std::cout<<exec(command);
+
+	std::ifstream inp;
+	inp.open("C:\\Users\\FYP\\diff.txt");
+	std::getline(inp, ret);
+	std::getline(inp, ret);
+	std::cout<<"return :: " << ret << "\n";
+	size_t temp = ret.find("no differences encountered");
+
+	if(temp!=std::string::npos)
+	{
+		std::cout<<"Success";
+		changeEvaPosition(level);
+		return 1;
+	}
+	else
+	{
+		std::cout<<"Failure";
+		return 0;
+	}
+}
+
+
+
+#endif
